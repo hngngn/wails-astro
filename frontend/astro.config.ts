8387df89ad625f7fa644cc5a9464c0d10b617860
hnggngn <rozephyrus@gmail.com>
2023-01-21 20:50:21 +0700

    revert to plain astro

--- frontend/astro.config.ts
+++ frontend/astro.config.ts
@@ -1,4 +1,3 @@
-import solidJs from "@astrojs/solid-js"
 import compress from "astro-compress"
 import fonts from "astro-fonts-next"
 import { defineConfig } from "astro/config"
@@ -9,7 +8,6 @@ export default defineConfig({
     integrations: [
         unocss(),
         compress(),
-        solidJs(),
         fonts({
             url: "https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap",
         }),
